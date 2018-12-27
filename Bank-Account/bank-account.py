89253f0603431eed9817a0c8d9bf6c055e7fd176
Alex Navarro <alexjandron99@gmail.com>
2018-12-27 13:40:31 +0100

    withdraw created

--- Bank-Account/bank-account.py
+++ Bank-Account/bank-account.py
@@ -1,9 +1,25 @@
 class BankAccount():
-    def __init__(self, name, surnames, address, phone_number):
+    def __init__(self, name, surnames, address, phone_number, nif, balance):
         self.name = name
         self.surnames = surnames
         self.address = address
         self.phone_number = phone_number
+        self.nif = nif
+        self.balance = balance
+
+    def set():
+        pass
+    
+    def get():
+        pass
+
+    def withdrawMoney(self, money):
+        if money <= self.balance:
+            self.balance -= money
+        else:
+            raise ValueError
+
+    
 
 
 
