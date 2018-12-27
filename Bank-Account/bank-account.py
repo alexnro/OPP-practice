96f2a97313f43a7788118e4465273ec9d93e3f28
Alex Navarro <alexjandron99@gmail.com>
2018-12-27 13:34:11 +0100

    function init

---
+++ Bank-Account/bank-account.py
@@ -0,0 +1,12 @@
+class BankAccount():
+    def __init__(self, name, surnames, address, phone_number):
+        self.name = name
+        self.surnames = surnames
+        self.address = address
+        self.phone_number = phone_number
+
+
+
+if __name__ == '__main__':
+
+    assert
