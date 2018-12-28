e12c9a89f4479c568ca4be68f63b12a4e2779c3e
Alex Navarro <alexjandron99@gmail.com>
2018-12-28 23:51:07 +0100

    setTime done

--- Set-Time/set-time.py
+++ Set-Time/set-time.py
@@ -5,6 +5,26 @@ class Time():
         self.minutes = 0
         self.seconds = 0
 
+    def setTime(self, hour, minutes, seconds):
+        if hour in range(0, 25):
+            self.hour = hour
+        else:
+            self.hour = 0
+        
+        if minutes in range(0, 60):
+            self.minutes = minutes
+        else:
+            self.minutes = 0
+
+        if seconds in range(0, 60):
+            self.seconds = seconds
+        else:
+            self.seconds = 0
+
+    def getTime(self.hour, self.minutes, self.seconds):
+        
+        
+
 
 if __name__ == '__main__':
 
