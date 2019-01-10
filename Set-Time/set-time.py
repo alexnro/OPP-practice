14395865ec8991ccaf4e5b3455bb981d9f14181e
Alex Navarro <alexjandron99@gmail.com>
2019-01-10 13:27:34 +0100

    Casos test pasados

--- Set-Time/set-time.py
+++ Set-Time/set-time.py
@@ -1,36 +1,46 @@
 class Time():
 
+
     def __init__(self, hour, minutes, seconds):
         self.hour = 0
         self.minutes = 0
         self.seconds = 0
 
+
     def setTime(self, hour, minutes, seconds):
         if hour in range(0, 25):
             self.hour = hour
         else:
-            self.hour = 0
+            self.hour = 00
         
         if minutes in range(0, 60):
             self.minutes = minutes
         else:
-            self.minutes = 0
+            self.minutes = 00
 
         if seconds in range(0, 60):
             self.seconds = seconds
         else:
-            self.seconds = 0
+            self.seconds = 00
+
 
     def getTime(self):
         return f'{self.hour}:{self.minutes}:{self.seconds}'
 
+
     def printTime(self):
-        print(getTime())
+        print(self.getTime())
 
-
-actualHour = Time() 
+ 
 
 if __name__ == '__main__':
 
+
+    actualHour = Time(1, 2, 3)
     actualHour.setTime(2, 25, 36)
-    actualHour.getTime()
+    assert actualHour.getTime() == "2:25:36"
+
+    actualHour.setTime(25, 30, 30)
+    assert actualHour.getTime() == "0:30:30"
+    actualHour.setTime(2, 60, 60)
+    assert actualHour.getTime() == "2:0:0"
